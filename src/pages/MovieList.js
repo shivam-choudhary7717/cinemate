a825c835db17c183ec0bb3da2e52ca1c491ba4df
shivam-choudhary7717 <shivamchoudhary5602@gmail.com>
2023-06-26 17:42:32 +0530

    new updated code

--- src/pages/MovieList.js
+++ src/pages/MovieList.js
@@ -9,7 +9,7 @@ export const MovieList = ({ apiPath, title }) => {
 
     return (
         <main>
-            <section className="max-w-6xl mx-auto py-7">
+            <section className="max-w-7xl mx-auto py-7">
                 <div className="flex justify-start flex-wrap other:justify-evenly">
                     {movies.map((movie) => (
                         <Card key={movie.id} movie={movie} />
